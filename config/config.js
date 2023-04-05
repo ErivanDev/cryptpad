49ee319ff73839be1d08d1f28396867517822c0a
ErivanDev <gasparzinho633@gmail.com>
2023-04-05 20:42:46 -0300

    safe

--- config/config.js
+++ config/config.js
@@ -67,7 +67,7 @@ module.exports = {
  *
  *  CUSTOMIZE AND UNCOMMENT THIS FOR PRODUCTION INSTALLATIONS.
  */
-    // httpSafeOrigin: "https://some-other-domain.xyz",
+    httpSafeOrigin: process.env.RENDER_EXTERNAL_URL,
 
 /*  httpAddress specifies the address on which the nodejs server
  *  should be accessible. By default it will listen on 127.0.0.1
